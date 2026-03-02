0350a8d5bfab7c19d9b9740086a2a2dd0d86cba5
Stanislava Mladenovska <stanislava.mladenovska@netcetera.com>
2026-03-02 20:24:05 +0100

    Initial Commit

---
+++ SmartGymTests/SmartGymTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartGymTests.swift
+//  SmartGymTests
+//
+//  Created by Stanislava Mladenovska on 2.3.26.
+//
+
+import Testing
+@testable import SmartGym
+
+struct SmartGymTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
